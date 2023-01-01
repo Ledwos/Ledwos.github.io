593bbd00e2cda60b539962ecbbc92c677a199bfc
Ledwos <32643853+Ledwos@users.noreply.github.com>
2023-01-01 20:18:33 +0000

    Updates

--- static/js/main.bd28d0ea.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpledwos-front"]=this["webpackJsonpledwos-front"]||[]).push([[0],{12:function(t,e,n){},14:function(t,e,n){"use strict";n.r(e);n(0);var o=n(3),c=n.n(o),r=(n(12),n(1));var i=function(){return Object(r.jsx)("div",{children:"less boiler plate"})},s=function(t){t&&t instanceof Function&&n.e(3).then(n.bind(null,15)).then((function(e){var n=e.getCLS,o=e.getFID,c=e.getFCP,r=e.getLCP,i=e.getTTFB;n(t),o(t),c(t),r(t),i(t)}))};c.a.createRoot(document.getElementById("root")).render(Object(r.jsx)(i,{})),s()}},[[14,1,2]]]);
-//# sourceMappingURL=main.bd28d0ea.chunk.js.map
